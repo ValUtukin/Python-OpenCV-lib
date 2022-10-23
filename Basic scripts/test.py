6e919c0ae17bee13002df3fcf060167ae8c60b7f
Valery <swimingval@gmail.com>
2022-10-24 02:08:24 +0700

    CS fix

--- Basic scripts/test.py
+++ Basic scripts/test.py
@@ -1,14 +1,4 @@
 import cv2 as cv
-import web_cam as wc
-import hystogram as hysto
-
-video_cap = cv.VideoCapture(0)
-
-img = wc.grab_frame_from_camera(video_cap)
-cv.imshow('Frame from Camera', img)
-
-hysto.show_gray_histogram(img)
-hysto.show_color_histogram(img)
 
 cv.waitKey(0)
 
